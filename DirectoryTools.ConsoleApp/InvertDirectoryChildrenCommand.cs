85b4c6911dfc08cdc083a29f17a8b1dd37d8e8c5
Kieran Coote-Dinh <Kieran.Coote-Dinh@MedicalDirector.com>
2018-12-11 11:37:54 +1100

    Add dry run of invert command

--- DirectoryTools.ConsoleApp/InvertDirectoryChildrenCommand.cs
+++ DirectoryTools.ConsoleApp/InvertDirectoryChildrenCommand.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.Extensions.CommandLineUtils;
+using Serilog;
+using Serilog.Core;
 
 namespace DirectoryTools.ConsoleApp
 {
@@ -36,6 +41,65 @@ namespace DirectoryTools.ConsoleApp
 
         public void Run()
         {
+            // get target dir
+            var targetDirectoryInfo = new DirectoryInfo(ParentPath);
+
+            // verify exists
+            if (!targetDirectoryInfo.Exists) return;
+
+            // get direct children
+            var directChildren = targetDirectoryInfo.GetDirectories();
+
+            // get all 2nd level children
+            var secondLevelChildren = directChildren.SelectMany(childDir => childDir.GetDirectories());
+            
+            // determine new direct children
+            var newDirectChildrenNames = 
+                secondLevelChildren.Select(secondLevelChild => secondLevelChild.Name)
+                    .Distinct(StringComparer.InvariantCultureIgnoreCase);
+            
+            // determine new 2nd level children?
+            
+            // Create new child path and then move to current after all move operations
+            // get all 3rd levels and rewrite paths?
+            foreach (var secondLevelChild in secondLevelChildren)
+            {
+                foreach (var thirdLevelChild in secondLevelChild.GetDirectories())
+                {
+                    // move third level child to new home
+                    // need to consider dry run here
+                    var oldPath = thirdLevelChild.FullName;
+
+                    var oldDirectName = secondLevelChild.Parent.Name;
+                    var oldSecondLevelName = secondLevelChild.Name;
+                    var newPath = Path.Combine(targetDirectoryInfo.FullName, oldSecondLevelName, oldDirectName, thirdLevelChild.Name);
+
+                    Log.Information($"Move: {oldPath} => {newPath}");
+                    
+                    if (!DryRun)
+                    {
+                        // action
+                    }
+                }
+                foreach (var thirdLevelChild in secondLevelChild.GetFiles())
+                {
+                    // move third level child to new home
+                    // need to consider dry run here
+                    var oldPath = thirdLevelChild.FullName;
+
+                    var oldDirectName = secondLevelChild.Parent.Name;
+                    var oldSecondLevelName = secondLevelChild.Name;
+                    var newPath = Path.Combine(targetDirectoryInfo.FullName, oldSecondLevelName, oldDirectName, thirdLevelChild.Name);
+
+                    Log.Information($"Move: {oldPath} => {newPath}");
+                    
+                    if (!DryRun)
+                    {
+                        // action
+                    }
+                }
+            }
+            // go?
             Console.WriteLine($"{nameof(InvertDirectoryChildrenCommand)} OnExecute");
             Console.WriteLine($"{ParentPath}");
             Console.WriteLine(DryRun ? "Yes" : "No");
