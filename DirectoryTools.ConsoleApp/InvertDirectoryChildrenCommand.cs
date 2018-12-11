c46d46a9b90398d7fa589b40fd50c28fdbbc6953
Kieran Coote-Dinh <Kieran.Coote-Dinh@MedicalDirector.com>
2018-12-11 11:48:31 +1100

    Remove unused usings

--- DirectoryTools.ConsoleApp/InvertDirectoryChildrenCommand.cs
+++ DirectoryTools.ConsoleApp/InvertDirectoryChildrenCommand.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.Extensions.CommandLineUtils;
 using Serilog;
-using Serilog.Core;
 
 namespace DirectoryTools.ConsoleApp
 {
